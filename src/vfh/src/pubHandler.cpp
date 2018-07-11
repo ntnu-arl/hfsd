e0155334299e67b76858bb7d4e9c99f5b9ec727f
Ryan Fite <ryanfite@live.com>
2018-07-11 12:27:54 -0700

    Fixed the transformations into the sliding window by converting all items from ROS tf2 to Eigen

--- src/vfh/src/pubHandler.cpp
+++ src/vfh/src/pubHandler.cpp
@@ -34,24 +34,56 @@ void pubHandler::messageReceivedCloud(const pcl::PointCloud<pcl::PointXYZ>::Cons
 }
 
 //this recieves the odometry for the program to create the sliding window
-void pubHandler::messageReceivedPose(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg){
+void pubHandler::messageReceivedPose(const nav_msgs::Odometry::ConstPtr& msg){
 	ROS_INFO("Receiving Odometry...");
+	nav_msgs::Odometry odomData = *msg;
 	if(_window.size() == _queueSize){
 		_window.pop_back();
+		_odomWindow.pop_back();
 	}
 	_window.push_front(_data);
+	_odomWindow.push_front(odomData);
 	//ROS_INFO("Check 2");
-	pcl::PointCloud<pcl::PointXYZ> ptCloudScene = pcl::PointCloud<pcl::PointXYZ>(_preprocessing(_window));
+	pcl::PointCloud<pcl::PointXYZ> ptCloudScene = pcl::PointCloud<pcl::PointXYZ>(_preprocessing(_window, _odomWindow));
 	//ROS_INFO("Check 3");
 	this->publish(ptCloudScene);
 	//ROS_INFO("Check 4");
 }
 
+//takes in two quaternions and gives back the quaternion that rotates from the starting quaternion to the next quaternion
+Eigen::Quaterniond pubHandler::differenceOfQuat(Eigen::Quaterniond start, Eigen::Quaterniond end){
+	Eigen::Quaterniond difference = start.inverse() * end;
+	return difference;
+}
+Eigen::Vector3d pubHandler::differenceOfVec(Eigen::Vector3d start, Eigen::Vector3d end){
+	Eigen::Vector3d difference = start-end;
+	return difference;
+}
+
+
 //this is the pre-processing step that transforms and filters the point cloud queue
-pcl::PointCloud<pcl::PointXYZ> pubHandler::_preprocessing(std::deque<pcl::PointCloud<pcl::PointXYZ> > window){
+pcl::PointCloud<pcl::PointXYZ> pubHandler::_preprocessing(std::deque<pcl::PointCloud<pcl::PointXYZ> > window, std::deque<nav_msgs::Odometry> odomWindow){
 	pcl::PointCloud<pcl::PointXYZ> ptCloudScene = pcl::PointCloud<pcl::PointXYZ>(window[0]);
+
+	Eigen::Vector3d endV;
+	Eigen::Vector3d startV;
+	Eigen::Quaterniond endQ;
+	Eigen::Quaterniond startQ;
+
+	tf2::fromMsg(odomWindow[0].pose.pose.position, endV);
+	tf2::fromMsg(odomWindow[0].pose.pose.orientation,endQ);
 	for(int i = 1; i<window.size();i++){
-		ptCloudScene+=window[i];
+		tf2::fromMsg(odomWindow[i].pose.pose.position, startV);
+		tf2::fromMsg(odomWindow[i].pose.pose.orientation,startQ);
+		Eigen::Quaterniond differenceQ = differenceOfQuat(startQ,endQ);
+		Eigen::Vector3d differenceV = differenceOfVec(startV,endV);
+		differenceV = differenceV.transpose() * endQ.toRotationMatrix();
+		Eigen::Affine3d affine = Eigen::Affine3d::Identity();;
+		affine.translation() << differenceV[0],differenceV[1],differenceV[2];
+		affine.rotate(differenceQ.toRotationMatrix());
+		pcl::PointCloud<pcl::PointXYZ>::Ptr transformedCloud(new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::transformPointCloud(window[i],*transformedCloud,affine);
+		ptCloudScene+= *transformedCloud;
 	}
 	ROS_INFO("SUCCESS");
 	return ptCloudScene;
--- src/vfh/src/pubHandler.h
+++ src/vfh/src/pubHandler.h
@@ -11,8 +11,15 @@
 
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
+#include "nav_msgs/Odometry.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_eigen/tf2_eigen.h"
+#include "Eigen/Geometry"
 #include "pcl_ros/point_cloud.h"
+#include "pcl_ros/transforms.h"
+#include "pcl/common/transforms.h"
 #include "pcl_conversions/pcl_conversions.h"
+#include "tf2/LinearMath/Quaternion.h"
 
 #include <iostream>
 #include "trajectory.h"
@@ -21,8 +28,10 @@ class pubHandler{
 public:
 	pubHandler(ros::NodeHandle n, const std::string& s, int num);
 	void messageReceivedCloud(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg);
-	void messageReceivedPose(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg);
+	void messageReceivedPose(const nav_msgs::Odometry::ConstPtr& msg);
 	void publish(pcl::PointCloud<pcl::PointXYZ> msg);
+	Eigen::Quaterniond differenceOfQuat(Eigen::Quaterniond start, Eigen::Quaterniond end);
+	Eigen::Vector3d differenceOfVec(Eigen::Vector3d start, Eigen::Vector3d end);
 	//pcl::PointCloud<pcl::PointXYZ>::ConstPtr& getData();
 
 private:
@@ -30,10 +39,11 @@ private:
 	ros::Publisher _pub;
 	pcl::PointCloud<pcl::PointXYZ> _data;
 	std::deque<pcl::PointCloud<pcl::PointXYZ> > _window;
+	std::deque<nav_msgs::Odometry> _odomWindow;
 	int _queueSize;
 	/*Private Functions*/
 	std::map<std::string,std::vector<trajectory> > _vfh3D();
-	pcl::PointCloud<pcl::PointXYZ> _preprocessing(std::deque<pcl::PointCloud<pcl::PointXYZ> > window);
+	pcl::PointCloud<pcl::PointXYZ> _preprocessing(std::deque<pcl::PointCloud<pcl::PointXYZ> > window,std::deque<nav_msgs::Odometry> odomWindow);
 };
 
 #endif /* PUBHANDLER_H_ */
